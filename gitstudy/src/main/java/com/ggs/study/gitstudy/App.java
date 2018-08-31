df4d12c66c3bf83ed317b8859974ac0030ea2b90
geguis <geguisong@sina.com>
2018-08-31 09:46:38 +0800

    insert integer compare

--- gitstudy/src/main/java/com/ggs/study/gitstudy/App.java
+++ gitstudy/src/main/java/com/ggs/study/gitstudy/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
+    	Integer a = new Integer(1);
+    	Integer b = new Integer(1);
+    	System.out.println(a==b);
+    	System.out.println(a.equals(b));
         System.out.println( "Hello World!" );
     }
 }
