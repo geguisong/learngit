9f4cf7b6dd49fce8dea1121c24b3099b622718e5
geguis <geguisong@sina.com>
2018-08-31 09:37:22 +0800

    study project add

---
+++ gitstudy/src/main/java/com/ggs/study/gitstudy/App.java
@@ -0,0 +1,13 @@
+package com.ggs.study.gitstudy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
